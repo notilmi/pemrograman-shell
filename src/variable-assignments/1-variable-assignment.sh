da9db228d82dac52d5149dd6f62af9d4db631c85
Achmad Ilmi <dznilmi@gmail.com>
2025-07-04 15:36:48 +0700

    Add comments explaining variable assignment scripts

--- src/variable-assignments/1-variable-assignment.sh
+++ src/variable-assignments/1-variable-assignment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 name="Ilmi"
-echo "Your name is $name"
+echo "Your name is $name" # Your name is Ilmi
--- src/variable-assignments/2-variable-assignment-io.sh
+++ src/variable-assignments/2-variable-assignment-io.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Prompt ke pengguna
 echo "What is your name?"
 
+# Menangkap Input dengan
+# nama variabel `name`
 read name
 
 echo "Hello, $name!"
